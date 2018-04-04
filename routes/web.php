44571b591144b885f942bfb36131d5857be75b40
Deangelles Debastiani Cordeiro <deangelles123456@gmail.com>
2018-04-03 22:08:27 -0400

    Estrutura inicial 03/04/2016

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,15 +9,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/produtos/pesquisar', 'ProdutoController@pesquisar');
-
 Route::post('/produtos/pesquisar', 'ProdutoController@pesquisar');
-
 Route::get('/produtos/inserir', 'ProdutoController@mostrar_inserir');
-
 Route::post('/produtos/inserir', 'ProdutoController@inserir');
+Route::get('/produtos/alterar/{id}', 'ProdutoController@mostrar_alterar');
+Route::post('/produtos/alterar', 'ProdutoController@alterar');
+Route::get('/produtos/excluir/{id}', 'ProdutoController@excluir');
